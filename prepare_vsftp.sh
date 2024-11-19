23e219ce885d5c1832c1909be533c83ca028634c
Milad Nasiri <milad@targeteagletechnologies.com>
2024-11-19 21:44:51 +0330

    update script

--- prepare_vsftp.sh
+++ prepare_vsftp.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Function to install vsftpd
-#install_vsftpd() {
-#  read -p "Enter the user name: " ftp_user
-#  read -sp "Enter the password for $ftp_user: " ftp_password
-#  read -p "Enter the root directory for FTP: " ftp_root
-#
-#  # Install vsftpd
-#  sudo apt-get update -y
-#  sudo apt-get install vsftpd -y
-#
-#  # Configure vsftpd
-#  sudo nano /etc/vsftpd.conf
-#
-#  # Add the following lines to the configuration file:
-#  # (Replace placeholders with actual values)
-#  anon_root=/srv/ftp
-#  local_enable=YES
-#  write_enable=YES
-#  local_umask=022
-#  pasv_min_port=40000
-#  pasv_max_port=40099
-#
-#  # Create the FTP user and set the password
-#  sudo useradd $ftp_user
-#  sudo passwd $ftp_user
-#
-#  # Set the user's home directory and permissions
-#  sudo mkdir -p $ftp_root
-#  sudo chown $ftp_user:$ftp_user $ftp_root
-#  sudo chmod 755 $ftp_root
-#
-#  # Restart vsftpd
-#  sudo systemctl restart vsftpd
-#}
+install_vsftpd() {
+  read -p "Enter the user name: " ftp_user
+  read -sp "Enter the password for $ftp_user: " ftp_password
+  read -p "Enter the root directory for FTP: " ftp_root
+
+  # Install vsftpd
+  sudo apt-get update -y
+  sudo apt-get install vsftpd -y
+
+  # Configure vsftpd
+  sudo nano /etc/vsftpd.conf
+
+  # Add the following lines to the configuration file:
+  # (Replace placeholders with actual values)
+  anon_root=/srv/ftp
+  local_enable=YES
+  write_enable=YES
+  local_umask=022
+  pasv_min_port=40000
+  pasv_max_port=40099
+
+  # Create the FTP user and set the password
+  sudo useradd $ftp_user
+  sudo passwd $ftp_user
+
+  # Set the user's home directory and permissions
+  sudo mkdir -p $ftp_root
+  sudo chown $ftp_user:$ftp_user $ftp_root
+  sudo chmod 755 $ftp_root
+
+  # Restart vsftpd
+  sudo systemctl restart vsftpd
+}
 
 function main_menu {
 	local option
@@ -42,16 +42,14 @@ function main_menu {
 	echo "	1) install vsftp"
 	echo "	0) Exit"
 	read -r -p "Please enter an option: " option
-#	case $option in
-#	1) install_vsftpd ;;
-#  0) exit 0;;
-#	esac
+	case $option in
+	1) install_vsftpd ;;
+  0) exit 0;;
+  *) echo "Invalid option. Please enter 1 or 0." ;;
+	esac
 }
 
 
 # Main menu
 clear
-echo "V2Fly/Xray installer script by Hirbod Behnam"
-echo "Source at https://github.com/HirbodBehnam/V2Ray-Installer"
-echo
 main_menu
